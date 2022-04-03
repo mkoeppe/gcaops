2d67e8f296e6101da9a00774741e6b795b772bc2
Ricardo Buring <ricardo.buring@gmail.com>
2022-04-03 21:17:38 +0200

    UndirectedGraph: add examples in documentation

--- gcaops/graph/undirected_graph.py
+++ gcaops/graph/undirected_graph.py
@@ -18,6 +18,18 @@ class UndirectedGraph:
         - ``num_vertices`` -- a natural number, the number of vertices
 
         - ``edges`` -- a list of tuples of natural numbers
+
+        EXAMPLES:
+
+        #. Construct the graph consisting of a single edge::
+
+            sage: g = UndirectedGraph(2, [(0, 1)]); g
+            UndirectedGraph(2, [(0, 1)])
+
+        #. Construct the tetrahedron graph::
+
+            sage: g = UndirectedGraph(4, [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]); g
+            UndirectedGraph(4, [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)])
         """
         if not num_vertices >= 0:
             raise ValueError('num_vertices must be a natural number')
@@ -37,36 +49,80 @@ class UndirectedGraph:
     def __repr__(self):
         """
         Return a string representation of this graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(3, [(0, 1), (1, 2), (2, 0)])
+            sage: repr(g)
+            'UndirectedGraph(3, [(0, 1), (1, 2), (0, 2)])'
         """
         return 'UndirectedGraph({}, {})'.format(self._num_vertices, self._edges)
 
     def __len__(self):
         """
         Return the number of vertices of this graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(3, [(0, 1), (1, 2), (2, 0)])
+            sage: len(g)
+            3
         """
         return self._num_vertices
 
     def __eq__(self, other):
         """
         Return ``True`` if this graph equals ``other``.
+
+        Note that this is *not* an isomorphism test, and the ordering of the list of edges is taken into account.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(3, [(0, 1), (1, 2), (2, 0)])
+            sage: h1 = UndirectedGraph(3, [(0, 1), (1, 2), (0, 2)])
+            sage: g == h1
+            True
+            sage: h2 = UndirectedGraph(3, [(0, 1), (0, 2), (1, 2)])
+            sage: g == h2
+            False
         """
         return isinstance(other, self.__class__) and self._num_vertices == other._num_vertices and self._edges == other._edges
 
     def edges(self):
         """
         Return the list of edges of this graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(3, [(0, 1), (1, 2)])
+            sage: g.edges()
+            [(0, 1), (1, 2)]
         """
         return self._edges
 
     def canonicalize_edges(self):
         """
         Lexicographically order the edges of this graph and return the sign of that edge permutation.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(3, [(1, 2), (0, 1)])
+            sage: g.canonicalize_edges()
+            -1
+            sage: g
+            UndirectedGraph(3, [(0, 1), (1, 2)])
         """
         return selection_sort(self._edges)
 
     def relabeled(self, relabeling):
         """
-        Return a vertex relabeling of this graph.
+        Return the graph obtained by relabeling this graph in the given way.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(3, [(0, 1), (1, 2)])
+            sage: g.relabeled({0: 1, 1: 0, 2: 2})
+            UndirectedGraph(3, [(0, 1), (0, 2)])
         """
         new_edges = [(relabeling[a], relabeling[b]) for (a,b) in self._edges]
         # constructor takes care of canonicalizing individual edges:
@@ -74,7 +130,13 @@ class UndirectedGraph:
 
     def orientations(self):
         """
-        An iterator producing the DirectedGraphs which are obtained by orienting this graph in all possible ways.
+        Return a generator producing the DirectedGraphs which are obtained by orienting this graph in all possible ways.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: list(g.orientations())
+            [DirectedGraph(2, [(0, 1)]), DirectedGraph(2, [(1, 0)])]
         """
         from .directed_graph import DirectedGraph
         num_edges = len(self._edges)
@@ -85,11 +147,19 @@ class UndirectedGraph:
 
     def _insertion_graphs(self, position, other):
         """
-        An iterator producing the graphs which are obtained by inserting ``other`` into the vertex ``position`` of this graph.
+        Return a generator producing the graphs which are obtained by inserting ``other`` into the vertex ``position`` of this graph.
 
         .. NOTE::
 
             The convention used is that the edges which originate from ``other`` are last in the edge ordering of each produced graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: list(g._insertion_graphs(0, g))
+            [UndirectedGraph(3, [(0, 2), (0, 1)]), UndirectedGraph(3, [(1, 2), (0, 1)])]
+            sage: list(g._insertion_graphs(1, g))
+            [UndirectedGraph(3, [(0, 1), (1, 2)]), UndirectedGraph(3, [(0, 2), (1, 2)])]
         """
         # relabel user (vertices > position are shifted to make room for victim)
         user_edges = [[a + len(other) - 1 if a > position else a, b + len(other) - 1 if b > position else b] for (a,b) in self.edges()]
@@ -108,11 +178,19 @@ class UndirectedGraph:
 
     def _expanding_differential_graphs(self):
         """
-        An iterator producing the graphs which are obtained in the vertex-expanding differential of this graph.
+        Return a generator producing the graphs which are obtained in the vertex-expanding differential of this graph.
 
         .. NOTE::
 
             The convention used is that the new edge is last in the edge ordering of each produced graph.
+            For optimization purposes, graphs which will definitely cancel out in the differential are not produced.
+            This is the case e.g. for graphs with "leaves" created by the insertion of the stick graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: list(g._expanding_differential_graphs())
+            []
         """
         for position in range(self._num_vertices):
             # relabel user (vertices > position are shifted to make room for stick)
@@ -137,6 +215,12 @@ class UndirectedGraph:
     def _sage_(self):
         """
         Return a Sage version of this graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: g._sage_()
+            Graph on 2 vertices
         """
         from sage.graphs.graph import Graph
         return Graph([(a,b,i) for (i,(a,b)) in enumerate(self.edges())])
@@ -144,18 +228,42 @@ class UndirectedGraph:
     def get_pos(self):
         """
         Return the dictionary of positions of vertices in this graph (used for plotting).
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: g.get_pos() is None
+            True
+            sage: g.set_pos({0: (0.0, 0.0), 1: (1.0, 0.0)})
+            sage: g.get_pos()
+            {0: (0.000000000000000, 0.000000000000000),
+             1: (1.00000000000000, 0.000000000000000)}
         """
         return self._vertex_positions
 
     def set_pos(self, new_pos):
         """
         Set the positions of vertices in this graph (used for plotting).
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: g.set_pos({0: (0.0, 0.0), 1: (1.0, 0.0)})
+            sage: g.get_pos()
+            {0: (0.000000000000000, 0.000000000000000),
+             1: (1.00000000000000, 0.000000000000000)}
         """
         self._vertex_positions = new_pos
 
     def plot(self, **options):
         """
         Return a plot of this graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: g.plot()
+            Graphics object consisting of 4 graphics primitives
         """
         g = self._sage_()
         from sage.graphs.graph_plot import GraphPlot
@@ -170,6 +278,11 @@ class UndirectedGraph:
     def show(self, **options):
         """
         Show this graph.
+
+        EXAMPLES::
+
+            sage: g = UndirectedGraph(2, [(0, 1)])
+            sage: g.show()
         """
         from sage.graphs.graph_plot import graphplot_options
         plot_options = {k: options.pop(k) for k in graphplot_options if k in options}
